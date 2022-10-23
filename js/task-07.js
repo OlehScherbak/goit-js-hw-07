37ba12cb268138bab1f811ff134d09247c385c16
Oleh.Scherbak <oleh.scherbak@ukr.net>
2022-10-23 22:54:45 +0300

    initial commit

--- js/task-07.js
+++
@@ -1,5 +0,0 @@
-const valueSetter = document.querySelector('#font-size-control');
-const text = document.querySelector('#text');
-valueSetter.addEventListener('input', event => {
-  text.style.fontSize = `${event.currentTarget.value}px`;
-});
